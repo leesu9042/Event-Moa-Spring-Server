4a45f6497759cba8c95877571072e881356d09c3
dohwi <dohwe0978@gmail.com>
2024-11-05 17:21:04 +0900

    set :: project

---
+++ src/main/java/com/example/eventmoa/EventMoaApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventmoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventMoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventMoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eventmoa/EventMoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventmoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventMoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
