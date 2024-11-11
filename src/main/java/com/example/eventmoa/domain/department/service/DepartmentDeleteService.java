93a656b29a896c54f8e89e9c04ccfc7e22cb8724
dohwi <dohwe0978@gmail.com>
2024-11-11 13:50:52 +0900

    feat :: department delete service

---
+++ src/main/java/com/example/eventmoa/domain/department/service/DepartmentDeleteService.java
@@ -0,0 +1,30 @@
+package com.example.eventmoa.domain.department.service;
+
+import com.example.eventmoa.domain.department.exception.DepartmentNotFoundException;
+import com.example.eventmoa.domain.department.persistence.Department;
+import com.example.eventmoa.domain.department.persistence.repository.DepartmentRepository;
+import com.example.eventmoa.domain.user.exception.UnauthorizedUserException;
+import com.example.eventmoa.domain.user.facade.UserFacade;
+import com.example.eventmoa.domain.user.persistence.User;
+import com.example.eventmoa.domain.user.role.Role;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DepartmentDeleteService {
+    private final DepartmentRepository departmentRepository;
+    private final UserFacade userFacade;
+
+    @Transactional
+    public void delete(Long id) {
+        Department department = departmentRepository.findById(id).orElseThrow(DepartmentNotFoundException::new);
+        User user = userFacade.getCurrentUser();
+        if(!user.getId().equals(department.getUser().getId()) && !user.getRole().equals(Role.DEPARTMENT) && !user.getRole().equals(Role.MASTER)){
+            throw UnauthorizedUserException.EXCEPTION;
+        }
+
+        departmentRepository.deleteById(id);
+    }
+}
