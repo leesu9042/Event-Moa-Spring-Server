6f03de6547a9cbf6373f016198b4d864c7c65805
dohwi <dohwe0978@gmail.com>
2024-11-11 14:04:39 +0900

    feat :: dong query all service

---
+++ src/main/java/com/example/eventmoa/domain/dong/service/DongQueryAllService.java
@@ -0,0 +1,24 @@
+package com.example.eventmoa.domain.dong.service;
+
+import com.example.eventmoa.domain.department.presentation.dto.response.DepartmentQueryAllResponse;
+import com.example.eventmoa.domain.dong.persistence.repository.DongRepository;
+import com.example.eventmoa.domain.dong.presentation.dto.response.DongQueryAllResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DongQueryAllService {
+    private final DongRepository dongRepository;
+
+    @Transactional(readOnly = true)
+    public List<DongQueryAllResponse> queryAll() {
+        return dongRepository.findAllByOrderByDateAscTimeAsc()
+                .stream()
+                .map(DongQueryAllResponse::new)
+                .toList();
+    }
+}
