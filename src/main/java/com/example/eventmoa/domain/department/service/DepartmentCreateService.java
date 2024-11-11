5774ccd1fd11b17c75bae5b6ff7f8f78e4f224f6
dohwi <dohwe0978@gmail.com>
2024-11-11 13:50:35 +0900

    feat :: department 추가

--- src/main/java/com/example/eventmoa/domain/department/service/DepartmentCreateService.java
+++ src/main/java/com/example/eventmoa/domain/department/service/DepartmentCreateService.java
@@ -3,6 +3,7 @@ package com.example.eventmoa.domain.department.service;
 import com.example.eventmoa.domain.department.persistence.Department;
 import com.example.eventmoa.domain.department.persistence.repository.DepartmentRepository;
 import com.example.eventmoa.domain.department.presentation.dto.request.DepartmentCreateRequest;
+import com.example.eventmoa.domain.user.facade.UserFacade;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class DepartmentCreateService {
     private final DepartmentRepository departmentRepository;
+    private final UserFacade userFacade;
     @Transactional
     public void create(DepartmentCreateRequest request) {
         departmentRepository.save(Department.builder()
@@ -21,6 +23,7 @@ public class DepartmentCreateService {
                         .personnel(request.getPersonnel())
                         .eventName(request.getEventName())
                         .time(request.getTime())
+                        .user(userFacade.getCurrentUser())
                 .build());
     }
 }
