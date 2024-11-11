c33b27bff78251ecfe37c7135809b4728e91fbd3
dohwi <dohwe0978@gmail.com>
2024-11-11 14:03:38 +0900

    feat :: dong entity

---
+++ src/main/java/com/example/eventmoa/domain/dong/persistence/Dong.java
@@ -0,0 +1,47 @@
+package com.example.eventmoa.domain.dong.persistence;
+
+import com.example.eventmoa.domain.department.Departments;
+import com.example.eventmoa.domain.dong.Dongs;
+import com.example.eventmoa.domain.user.persistence.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Table(name = "tbl_dong")
+public class Dong {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String eventName;
+
+    @Column(nullable = false)
+    private String date;
+
+    @Column(nullable = false)
+    private String time;
+
+    @Column(nullable = false)
+    private String place;
+
+    @Column(nullable = false)
+    private Long personnel;
+
+    @Column(nullable = false)
+    private Dongs dong;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Column(nullable = false)
+    private String phone;
+
+}
