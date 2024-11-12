69026836d0ccf2885e7e338458dc5cac12d167f6
dohwi <dohwe0978@gmail.com>
2024-11-12 17:20:02 +0900

    feat :: department queryhan response

---
+++ src/main/java/com/example/eventmoa/domain/department/presentation/dto/response/DepartmentQueryhanList.java
@@ -0,0 +1,37 @@
+package com.example.eventmoa.domain.department.presentation.dto.response;
+
+import com.example.eventmoa.domain.department.persistence.Department;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Getter
+public class DepartmentQueryhanList {
+    private String eventName;
+
+    @Schema(description = "행사 일정 / 24.4.7.(월) 형식으로 보낼것")
+    private String date;
+
+    @Schema(description = "행사 시각 / 09:00 형식으로 보낼것")
+    private String time;
+
+    @Schema(description = "행사 장소")
+    private String place;
+
+    @Schema(description = "행사 인원 int value")
+    private Long personnel;
+
+    @Schema(description = "행사 주관 과")
+    private String department;
+
+    @Schema(description = "비고")
+    private String note;
+
+    public DepartmentQueryhanList(Department department) {
+        this.eventName = department.getEventName();
+        this.date = department.getDate();
+        this.time = department.getTime();
+        this.place = department.getPlace();
+        this.personnel = department.getPersonnel();
+        this.department = department.getDepartment().getDepartmentName();
+    }
+}
---
+++ src/main/java/com/example/eventmoa/domain/department/presentation/dto/response/DepartmentQueryhanResponse.java
@@ -0,0 +1,12 @@
+package com.example.eventmoa.domain.department.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class DepartmentQueryhanResponse {
+    private List<DepartmentQueryhanList> data;
+}
